1717242c0e6e505c741b7d1f5347bfffbfee2fd3
Sajan <sajanr@vt.edu>
2018-10-08 16:26:29 -0400

    Added library file to contain methods and objects to be shared amongst the different components.

---
+++ lib.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import datetime
+
+def print_checkpoint(*msgs):
+    ''' Prints a checkpoint
+    Prints a timestamp and the provided message.
+    '''
+    time = datetime.datetime.now().time()
+    hour = '{0}'.format(str(time.hour).zfill(2))
+    minute = '{0}'.format(str(time.minute).zfill(2))
+    second = '{0}'.format(str(time.second).zfill(2))
+    time_format = '[' + hour + ':' + minute + ':' + second + ']'
+    print(time_format, *msgs)
+
+
