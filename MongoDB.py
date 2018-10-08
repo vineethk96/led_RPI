4874ce813bdf5bc730529535ff06d09c4e5bd1a3
Sajan <sajanr@vt.edu>
2018-10-08 16:53:53 -0400

    Added shebang lines to all files

--- MongoDB.py
+++ MongoDB.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 
 if __name__ == "__main__":
     print("This file encapsulates the MongoDB commands")
--- processor.py
+++ processor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import lib
 
 if __name__ == "__main__":
