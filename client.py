d1fc67cd0f4453130c0042da75102e2b7efaca35
Vineeth Kirandumkara <vineethk@vt.edu>
2018-10-17 15:41:12 -0400

    Added the RabbitMQ integration. Currently it is untested as it was written through gitLab. Testing to be done soon. Update client.py

--- client.py
+++ client.py
@@ -4,7 +4,40 @@ import lib
 import sys
 import argparse
 import json
+import pika
+import uuid
 
+#class was added on gitlab
+class rpcClient(object):
+    def __init__(self):
+       self.connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+        self.channel = self.connection.channel()
+
+        result = self.channel.queue_declare(exclusive=True)
+        self.callback_queue = result.method.queue
+
+        self.channel.basic_consume(self.on_response, no_ack=True, queue=self.callback_queue)
+
+    def on_response(self, ch, method, props, body):
+        if self.corr_id == props.correlation_id:
+            self.response = body
+
+    def call(self, n):
+        self.response = None
+        self.corr_id = str(uuid.uuid4())
+        self.channel.basic_publish(exchange='',
+                                   routing_key='rpc_queue',
+                                   properties=pika.BasicProperties(
+                                         reply_to = self.callback_queue,
+                                         correlation_id = self.corr_id,
+                                         ),
+                                   body=str(n))
+        while self.response is None:
+            self.connection.process_data_events()
+        return int(self.response)
+        
+#***************************************************
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -14,7 +47,6 @@ def parse_args():
     parser.add_argument("-count", dest="count")
 
     return parser.parse_args()
-
 
 if __name__ == "__main__":
     print("This is the client")
@@ -42,4 +74,8 @@ if __name__ == "__main__":
 
     else:
         #action is incorrect
-    
+        
+    # added from gitlab
+    client_rpc = rpcClient()
+    response = client_rpc.call(payload)
+    # decipher payload
