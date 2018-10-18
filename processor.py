63ffb37624218baceeae632efbd390b3770ba7d7
moha96 <moha96@vt.edu>
2018-10-18 16:53:05 -0400

    finished processor

--- processor.py
+++ processor.py
@@ -2,7 +2,7 @@
 import sys, bluetooth, pika
 from argparse import ArgumentParser
 from lib import print_checkpoint
-
+import json
 class BTConnect:
     def __init__(self, mac_addr, port, socket_size):
         self.mac_addr = mac_addr
@@ -12,10 +12,10 @@ class BTConnect:
         print_checkpoint("Connecting to ", self.mac_addr, " on port ", self.port, "\n")
         self.sock.connect((self.mac_addr, self.port))
     def send_message(self, payload):
-        self.sock.send(paylaod)
+        self.sock.send(payload)
         answer = self.sock.recv(self.socket_size)
-        print_checkpoint("Received answer payload: ", asnwer, "\n")
-        self.sock.close()
+        print_checkpoint("Received answer payload: ", answer.decode("utf-8"), "\n")
+        #self.sock.close()
         return answer
 
 class RPCServer:
@@ -23,7 +23,7 @@ class RPCServer:
         self.storage_addr = storage_addr
         self.storage_port = storage_port
         self.socket_size = socket_size
-        
+        self.storage_connect = BTConnect(self.storage_addr, self.storage_port, self.socket_size)
         self.mq_conn = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
         self.channel = self.mq_conn.channel()
 
@@ -37,13 +37,14 @@ class RPCServer:
         print_checkpoint("Awaiting client requests\n")
 
         self.channel.start_consuming()
-        
+        #self.storage_connect = BTConnect(self.storage_addr, self.storage_port, self.socket_size)
         
     def on_request(self, ch, method, props, body):
-        print_checkpoint("Received request payload: ", body, "\n")
-        self.storage_connect = BTConnect(self.storage_addr, self.storage_port, self.socket_size)
+        print_checkpoint("Received request payload: ", body.decode("utf-8"), "\n")
+        #self.storage_connect = BTConnect(self.storage_addr, self.storage_port, self.socket_size)
 
         self.response = self.storage_connect.send_message(body)
+        #self.response = "test"
         ch.basic_publish(exchange='',
                          routing_key=props.reply_to,
                          properties=pika.BasicProperties(correlation_id = \
@@ -51,7 +52,7 @@ class RPCServer:
                          body=self.response)
         ch.basic_ack(delivery_tag=method.delivery_tag)
         
-        del self.storage_connect
+       # del self.storage_connect
         
 def parse_args(args):
     parser = ArgumentParser()
@@ -68,6 +69,14 @@ if __name__ == "__main__":
         storage_addr = args.storage_addr
         storage_port = int(args.storage_port)
         socket_size = int(args.socket_size)
+        #test_BT = BTConnect(storage_addr, storage_port, socket_size)
+        #print("sending message")
+        #dict = {'msg': "Hello Sajan"} 
+        #st = json.dumps(dict)
+        #test_BT.send_message(st)
+        #del test_BT
+        #test_BT = BTConnect(storage_addr, storage_port, socket_size)
+        #test_BT.send_message("hello world 2")
         rpc_server = RPCServer(storage_addr, storage_port, socket_size)
     else:
         print("ERROR: Not enough arguments \n\nUsage: python3 processor.py -storage <Storage bluetooth MAC> -p <storage port> -z <socket size>\n")
